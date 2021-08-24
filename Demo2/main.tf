2f396d6fd28ec4ff42e5f29803b8373b79c26686
shaktidutta <89388878+shaktidutta@users.noreply.github.com>
2021-08-24 10:14:51 +0530

    Update main.tf

--- Demo2/main.tf
+++ Demo2/main.tf
@@ -1,9 +1,24 @@
-resource "aws_s3_bucket" "shaktidutta" {
-  bucket = "test-bucket"
-  acl    = "private"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "test_shakti_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
 
   tags = {
     Name        = "Shakti Bucket"
-    Environment = "Demo"
   }
 }
